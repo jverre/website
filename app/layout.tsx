41099975598e8ddc62cfe2360b1064c9d7ec21d4
Jacques <jacques.verre@stakion.io>
2025-02-28 21:50:58 +0000

    Fix SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,17 +48,17 @@ export const metadata: Metadata = {
     locale: 'en_US',
     type: 'website',
   },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //     'max-video-preview': -1,
+  //     'max-image-preview': 'large',
+  //     'max-snippet': -1,
+  //   },
+  // },
 }
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
